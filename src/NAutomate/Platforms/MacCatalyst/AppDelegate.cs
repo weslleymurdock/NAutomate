75081c231e7329098e1834c20cdc75cad6e701cc
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-09-24 19:28:07 -0300

    feat: Add projects

---
+++ src/NAutomate/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NAutomate;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
