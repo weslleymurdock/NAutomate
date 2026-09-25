06d4065f697b7a487ec1ea9a1b152a5f0956c5d6
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-25 02:56:47 -0300

    feat(ui): compose official modules and settings providers

--- src/NAutomate.Web/Program.cs
+++ src/NAutomate.Web/Program.cs
@@ -1,34 +1,31 @@
 using MudBlazor.Services;
 using NAutomate.Abstractions;
 using NAutomate.Core;
+using NAutomate.Modules;
+using NAutomate.Modules.Appium;
 using NAutomate.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MudBlazor services
 builder.Services.AddMudServices();
-builder.Services.AddSingleton<IModuleRegistry, ModuleRegistry>();
+builder.Services.AddSingleton<IModuleRegistry>(_ => new ModuleRegistry(OfficialModules.GetModules()));
 builder.Services.AddSingleton<WorkflowEngine>();
 builder.Services.AddSingleton<FileWorkflowStore>();
+builder.Services.AddSingleton<IModuleSettingsTabProvider, AppiumSettingsTabProvider>();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
