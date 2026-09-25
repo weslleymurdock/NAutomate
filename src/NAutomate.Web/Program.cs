112084f8cf356fa74c02cfb1f627a12a5832ea5d
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-25 12:01:08 -0300

    feat: validate host dependencies and map shared UI

--- src/NAutomate.Web/Program.cs
+++ src/NAutomate.Web/Program.cs
@@ -3,11 +3,27 @@ using NAutomate.Abstractions;
 using NAutomate.Core;
 using NAutomate.Modules;
 using NAutomate.Modules.Appium;
+using NAutomate.UI;
 using NAutomate.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(configuration =>
+{
+    configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+});
+
+builder.Services.AddNAutomateUI(dependencies =>
+{
+    dependencies.Add(new SoftwareDependencyDescriptor(
+        "powershell",
+        "PowerShell 7",
+        "pwsh",
+        ["--version"],
+        true,
+        "Install PowerShell 7 or configure the shell.pwsh module to use an available host runtime."));
+});
+
 builder.Services.AddSingleton<IModuleRegistry>(_ => new ModuleRegistry(OfficialModules.GetModules()));
 builder.Services.AddSingleton<WorkflowEngine>();
 builder.Services.AddSingleton<FileWorkflowStore>();
@@ -32,6 +48,7 @@ app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(NAutomate.UI.Components.Pages.Editor).Assembly);
 
 app.Run();
