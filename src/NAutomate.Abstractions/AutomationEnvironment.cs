9ea5df4bf28ff49fd593b2ab4ea0da124114836b
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-25 05:46:46 -0300

    feat(core): scope environments to automation workflows

--- src/NAutomate.Abstractions/AutomationEnvironment.cs
+++ src/NAutomate.Abstractions/AutomationEnvironment.cs
@@ -2,5 +2,6 @@ namespace NAutomate.Abstractions;
 
 public sealed record AutomationEnvironment(
     string Id,
+    string WorkflowName,
     string Name,
     IReadOnlyDictionary<string, string?> Values);
