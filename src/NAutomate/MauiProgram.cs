580ac7a84740a371b63e3fe3f5274388712df747
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-25 12:37:19 -0300

    feat: register MAUI project storage and shared UI

--- src/NAutomate/MauiProgram.cs
+++ src/NAutomate/MauiProgram.cs
@@ -4,7 +4,9 @@ using NAutomate.Abstractions;
 using NAutomate.Core;
 using NAutomate.Modules;
 using NAutomate.Modules.Appium;
-using NAutomate.Components;
+using NAutomate.UI;
+using NAutomate.Core.Projects;
+using Microsoft.Maui.Storage;
 
 namespace NAutomate;
 
@@ -19,8 +21,21 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-		builder.Services.AddMudServices();
+		builder.Services.AddMudServices(configuration =>
+		{
+			configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+		});
 		builder.Services.AddMauiBlazorWebView();
+
+		var projectsDirectory = Path.Combine(
+			FileSystem.AppDataDirectory,
+			"NAutomate",
+			"Projects");
+
+		builder.Services.AddSingleton<IAutomationProjectStore>(
+			_ => new FileAutomationProjectStore(projectsDirectory));
+
+		builder.Services.AddNAutomateUI();
 		builder.Services.AddSingleton<IModuleRegistry>(_ => new ModuleRegistry(OfficialModules.GetModules()));
 		builder.Services.AddSingleton<WorkflowEngine>();
 		builder.Services.AddSingleton<FileWorkflowStore>();
